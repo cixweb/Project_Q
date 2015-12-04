0ab54b5372b786c49a52e8ec6f8e49af4a4056ee
cixweb <diblasiogiovanni48@gmail.com>
2015-12-04 17:15:03 +0100

    cd

--- template/js/page_select.js
+++ template/js/page_select.js
@@ -9,4 +9,19 @@ $(document).ready(function() {
 		var c = $('#v2').attr('name');
 		$('#view').load(c);
 	});
+	
+	$("#v3").click(function() {
+		var c = $('#v3').attr('name');
+		$('#view').load(c);
+	});
+	
+	$("#v4").click(function() {
+		var c = $('#v4').attr('name');
+		$('#view').load(c);
+	});
+	
+	$("#v5").click(function() {
+		var c = $('#v5').attr('name');
+		$('#view').load(c);
+	});
 });
